8d1fcaf47fb28ee058cc9862cfd354835b1cf9cd
ViktorAtanasof <atanasowviktor@gmail.com>
2023-08-07 11:40:49 +0300

    Learn about redux behaviour of responding to actions not related to the certain reducer

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -90,6 +90,11 @@ const iceCreamReducer = (state = initialIceCreamState, action) => {
                 ...state,
                 numOfIceCreams: state.numOfIceCreams + action.payload,
             }
+        case CAKE_ORDERED:
+                return {
+                    ...state,
+                    numOfIceCreams: state.numOfIceCreams - 1,
+                }
         default:
             return state;
     }
